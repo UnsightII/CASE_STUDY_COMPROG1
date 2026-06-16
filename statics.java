b1fc7aaa94efe54774a9b159cf71f61ddbc4092e
Gar <johnandrewgarcia123@gmail.com>
2026-06-16 21:35:41 +0800

    Add static

---
+++ statics.java
@@ -0,0 +1,29 @@
+class Mobile{
+  String brand;
+  int price;
+  static String name;
+
+  public void Show(){
+    System.out.println(brand+" : "+price+" : "+name);
+  }
+
+}
+public class statics{
+  public static void main(String[]args){
+
+    Mobile obj1 = new Mobile();
+    obj1.brand = "Apple";
+    obj1.price = 10;
+    Mobile.name = "Smartphone";
+
+    Mobile obj2 = new Mobile();
+    obj2.brand = "Samsung";
+    obj2.price = 15;
+    Mobile.name = "Tablet";
+
+    Mobile.name = "Phone";
+
+    obj1.Show();
+    obj2.Show();
+  }
+}
